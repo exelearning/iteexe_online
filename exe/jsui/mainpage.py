a79650e7823ff1bf031e52f400fdd4a38125360a
ignaciogros <ignacio@gros.es>
2023-06-13 16:10:08 +0200

    i18n revision: Python syntax errors. Some strings were not translated.

--- exe/jsui/mainpage.py
+++ exe/jsui/mainpage.py
@@ -1319,7 +1319,7 @@ class MainPage(RenderableLivePage):
             Upload the exported package to elp Repository.
             """
 
-            uploading_package_message = _(u'Uploading package to {}...'.format(self.integration.repo_name))
+            uploading_package_message = _(u'Uploading package to {}...').format(self.integration.repo_name)
 
             # Update progress for the user
             client.call('Ext.MessageBox.updateProgress', 0.6, '50%', uploading_package_message)
@@ -1375,7 +1375,7 @@ class MainPage(RenderableLivePage):
                     return
 
                 if dict_response['status'] == '0':
-                    publish_document_message = _(u'Publishing document to {}'.format(self.integration.repo_name))
+                    publish_document_message = _(u'Publishing document to {}').format(self.integration.repo_name)
 
                     if dict_response['ode_id']:
                         self.package.ode_id = dict_response['ode_id']
@@ -1424,7 +1424,7 @@ class MainPage(RenderableLivePage):
                 client.call('Ext.MessageBox.hide')
                 error_args = e.args
                 if error_args and len(error_args[0]) == 2:
-                    client.alert(u'Error {}:{} '.format(*error_args[0])+_(u'when trying to upload package to {}.'.format(self.integration.repo_name)),
+                    client.alert(u'Error {}:{} '.format(*error_args[0])+_(u'when trying to upload package to {}.').format(self.integration.repo_name),
                                 title=publish_document_message)
                 else:
                     client.alert(u'Error {}'.format(e), title=publish_document_message)
